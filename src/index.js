4ee8bc6aa2a1fde4a3f2949f1f9fd7320c147f29
chenhan1 <chenhan1@360.cn>
2021-08-30 13:29:51 +0800

    Update v2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
+// import {
+//   render,
+//   createElement,
+//   Component
+// } from './lib/v1';
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     };
+//   }
+//   increase() {
+//     this.setState({
+//       count: this.state.count + 1
+//     });
+//   }
+//   render() {
+//     const { count } = this.state;
+//     return (
+//       <div>
+//         <h1>{count}</h1>
+//         <button onClick={e => this.increase()}>++++++</button>
+//       </div>
+//     );
+//   }
+// }
+
+// render(<App />, document.getElementById("app"));
+
 import {
   render,
   createElement,
-  Component
-} from './lib/index';
+  useState
+} from './lib/v2';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
-  increase() {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
-  render() {
-    const { count } = this.state;
-    return (
-      <div>
-        <h1>{count}</h1>
-        <button onClick={e => this.increase()}>++++++</button>
-      </div>
-    );
-  }
+function Counter() {
+  const [ state, setState ] = useState(1);
+  return (
+    <h1 onClick={() => setState(c => c + 1)}>
+      Count: {state}
+    </h1>
+  )
 }
-
-render(<App />, document.getElementById("app"));
+const element = <Counter />
+const container = document.getElementById("app")
+render(element, container)
